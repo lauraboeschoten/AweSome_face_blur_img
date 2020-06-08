bbe49cdf88731c6a94a80681a5381d41336cfeb2
lauraboeschoten <lauraboeschoten@gmail.com>
2020-06-08 16:21:44 +0200

    emotion output images in one .data file

--- code_img_emo_blur/Main.py
+++ code_img_emo_blur/Main.py
@@ -1,11 +1,14 @@
 import os
 import pandas as pd
+import numpy as np
+import json
+import pickle
 
 os.chdir('C:/code_img_emo_blur')
 
 import module_detect_faces as mdf
 import module_detect_emotions as mde
-import module_save_emotions as mse
+#import module_save_emotions as mse
 import module_blur_image as mbi
 
 # 0. make list with images
@@ -28,9 +31,20 @@ for i in range(len(path_list)):
     # 2. read emotions on detected faces
     lastlayer, prediction, emo_class = mde.detect_emotions(gray, boxes)
 
+
     # 3. make file with face location and emotion results
 
-    mse.save_emotions(boxes, lastlayer, prediction, emo_class, path_list[i])
+    emo_list = [boxes, prediction, emo_class, lastlayer]
+
+    #Save file to disk with pickle
+    with open(path_list[i][:-4] + '.data', 'wb') as filehandle:
+        # store the data as binary data stream
+        pickle.dump(emo_list, filehandle)
+
+    ##Load file from disk to workspace
+    #with open('emo_list_file.data', 'rb') as filehandle:
+    #    # read the data as binary data stream
+    #    emo_list_load = pickle.load(filehandle)
 
     # 4. blur image and save
 
--- code_img_emo_blur/module_save_emotions.py
+++ code_img_emo_blur/module_save_emotions.py
@@ -4,19 +4,29 @@ import json
 
 def save_emotions(boxes, lastlayer, prediction, emo_class, image):
 
-    emo_class = pd.DataFrame(emo_class, columns = ["classification"])
-    prediction = np.array(prediction)
-    m, n, r = prediction.shape
-    out_arr = np.column_stack((np.repeat(np.arange(m), n), prediction.reshape(m * n, -1)))
 
-    prediction = pd.DataFrame(out_arr)
-    boxes = pd.DataFrame(np.array(boxes))
+    emo_info = [boxes, prediction, emo_class, lastlayer]
+    emotest = np.array(emo_info)
+    #emo_array.append(emo_info)
 
-    df_emo_class = pd.concat([boxes, prediction.iloc[:, 1:8], emo_class], axis=1)
-    df_emo_class.columns = ['face_x', 'face_y', 'face_w', 'face_h',
-                            'pr_angry', 'pr_disgusted', 'pr_fearful', 'pr_happy', 'pr_neutral', 'pr_sad',
-                            'pr_surprised',
-                            'classification']
+    np.save(image[:-4] + ".npy", emo_info)
+    np.save("test.npy", emo_info)
 
-    df_emo_class.to_csv(image[:-4] + ".csv")
-    np.save(image[:-4] + "_lastlayer.npy", lastlayer)
+    return emo_info
+
+    #emo_class = pd.DataFrame(emo_class, columns = ["classification"])
+    #prediction = np.array(prediction)
+    #m, n, r = prediction.shape
+    #out_arr = np.column_stack((np.repeat(np.arange(m), n), prediction.reshape(m * n, -1)))
+
+    #prediction = pd.DataFrame(out_arr)
+    #boxes = pd.DataFrame(np.array(boxes))
+
+    #df_emo_class = pd.concat([boxes, prediction.iloc[:, 1:8], emo_class], axis=1)
+    #df_emo_class.columns = ['face_x', 'face_y', 'face_w', 'face_h',
+    #                        'pr_angry', 'pr_disgusted', 'pr_fearful', 'pr_happy', 'pr_neutral', 'pr_sad',
+    #                        'pr_surprised',
+    #                        'classification']
+
+    #df_emo_class.to_csv(image[:-4] + ".csv")
+    #np.save(image[:-4] + "_lastlayer.npy", lastlayer)
